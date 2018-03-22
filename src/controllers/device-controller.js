58930ca673aecfd502145cbd819e0effef46e2f7
@virmerson <virmerson@htcursos.com>
2018-03-22 16:24:04 -0400

    Removendo refreshToken e authenticate do device-controller

--- src/controllers/device-controller.js
+++ src/controllers/device-controller.js
@@ -4,7 +4,6 @@ const ValidationContract = require('../validators/fluent-validator');
 const SensorTypeValidator = require('../validators/sensor-type-validator');
 const deviceRepository = require('../repositories/device-repository');
 const contractRepository = require('../repositories/contract-repository');
-const sensorTypeRepository = require('../repositories/sensor-type-repository');
 const authService = require('../services/auth-service');
 
 /**
@@ -103,82 +102,5 @@ exports.getAll =  async (req, res, next)=>{
         res.status(200).send (devices);
     }catch(e){
         res.status(500).send({message:'Falha na requisição', data: e});
-    }
-};
-
-/**
-* Autentica o Dispositivo pelo Mac Adress 
-*/
-exports.authenticate = async(req, res, next) => {
-    try {
-        //Le o mac do dispositivo e valida se existe
-        const device = await deviceRepository.authenticate({
-            mac: req.body.mac
-        });
-        //Envia mensagem de erro se não encontrar dispositivo
-        if (!device) {
-            res.status(404).send({
-                message: 'Dispositivo Inválido ou Bloqueado'
-            });
-            return;
-        }
-        //Gera o token valido para o dispositivo
-        const token = await authService.generateToken({
-            id: device._id,
-            name: device.name, 
-            mac:req.body.mac
-        });
-        
-        //Envia o token para o dispositivo
-        res.status(201).send({
-            token: token,
-            data: {
-                id: device._id,
-                name: device.name 
-            }
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição', 
-            data:e
-        });
-    }
-};
-/**
-* 
-* Atualiza o token a cada novo request
-*/
-exports.refreshToken = async(req, res, next) => {
-    try {
-        const token = req.body.token || req.query.token || req.headers['x-access-token'];
-        const data = await authService.decodeToken(token);
-        
-        const device = await deviceRepository.getById(data.id);
-        
-        if (!device) {
-            res.status(404).send({
-                message: 'Dispositivo não encontrado'
-            });
-            return;
-        }
-        
-        const tokenData = await authService.generateToken({
-            id: device._id,
-            name: device.name,
-            mac:device.mac
-        });
-        
-        res.status(201).send({
-            token: tokenData,
-            data: {
-                name: device.name
-                
-            }
-        });
-    } catch (e) {
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
     }
 };

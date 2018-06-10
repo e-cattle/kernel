f2b86b156fdbcb97664636d8e492c8ea46cad518
Leandro Borges <leandroborges@outlook.com>
2018-06-10 18:08:46 -0400

    Commit da entidade AirTemperature

---
+++ src/models/air-temperature.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const airTemperature = new Schema({
+    uid:{
+        type:String,
+        required:true,
+        unique: true
+    }, 
+    mac:{
+        type:String,
+        required:true
+    },
+    value:{
+        type:Number,
+        required:true
+    },
+    unity: {
+        type:String,
+        default: "ºC",
+        required:true
+    },
+    dateRegister:{
+        type:Date,
+        default: Date.now,
+        required:true
+    },
+    dataStorage:{
+        type:Date,
+        default: Date.now,
+        required:true
+    },
+    syncedAt:{
+        type: Date        
+    },
+},{collection: "type-air-temperature"});
+
+module.exports =  mongoose.model ('type-air-temperature', airTemperature);

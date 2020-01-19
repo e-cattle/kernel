83fbb3c22794e7cf7f4fe65bf811a8f0bc5993ee
Bruno A. Caceres <brunoacaceres@gmail.com>
2020-01-18 22:44:11 -0400

    Retirada o parâmetro unique do model application e atualização da versão do kernel.

--- src/models/application.js
+++ src/models/application.js
@@ -1,42 +1,42 @@
-'use strict'
+"use strict";
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const application = new mongoose.Schema({
-  enable: {
-    type: Boolean,
-    required: true,
-    default: true,
+const application = new mongoose.Schema(
+  {
+    enable: {
+      type: Boolean,
+      required: true,
+      default: true
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    device: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    user: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    picture: {
+      type: String,
+      trim: true
+    }
   },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  device: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  user: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  picture: {
-    type: String,
-    trim: true,
-  },
-},
-{
-  timestamps: true,
-},
-)
+  {
+    timestamps: true
+  }
+);
 
-module.exports = mongoose.model('Application', application)
+module.exports = mongoose.model("Application", application);
